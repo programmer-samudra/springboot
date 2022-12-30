8cc21a747965dde3d64d801831a2e7225b356073
Samudra Sen <samudrasen.1983@gmail.com>
2022-12-30 16:25:37 +0800

    html integration

--- src/main/java/home/springboot/learning/DemoController.java
+++ src/main/java/home/springboot/learning/DemoController.java
@@ -1,14 +1,20 @@
 package home.springboot.learning;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    @Value("${spring.application.name}")
+    private String appname;
 
-    @GetMapping("")
-    public String hello() {
-        return "Hello World from SpringBoot!";
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("appName", appname);
+        return "Hello World From " + appname + "!";
+
     }
 
 }
